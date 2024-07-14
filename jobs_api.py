95758f2b672e73be3a2fca555135c4330316f369
vangog <vangog-2005@yandex.ru>
2024-07-14 17:53:22 +0300

    added error handlers

--- jobs_api.py
+++ jobs_api.py
@@ -3,12 +3,23 @@ from flask import jsonify, make_response
 
 from data import db_session
 from data.jobs import Jobs
+from main import app
 
 blueprint = flask.Blueprint(
     "jobs_api",
     __name__,
     template_folder="templates"
 )
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(400)
+def bad_request(_):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
 @blueprint.route("/api/jobs")
